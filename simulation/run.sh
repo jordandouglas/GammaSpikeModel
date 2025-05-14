14a7612b84ad5544756c37cfead30cc193061a9a
robertHyuan <101582763+sgtRoberty@users.noreply.github.com>
2025-05-14 17:36:22 +0200

    Add comments for birth-death stub tree simulator

--- simulation/run.sh
+++ simulation/run.sh
@@ -4,19 +4,10 @@ END=100
 
 cd templates
 
-
-
-for ((i=1;i<=END;i++)); do
-
+for ((i=1;i<=END;i++));
+do
 	echo $i
 	cd rep$i
 	~/beast/bin/beast -overwrite -df var.seq.json ../../run.xml
 	cd ../
-
-
 done
-
-
-
-
-
