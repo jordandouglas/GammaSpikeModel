b93a51ecfe26a84ad6943a5ff5a182e7081cd6a6
robertHyuan <101582763+sgtRoberty@users.noreply.github.com>
2025-09-17 19:07:03 +0200

    Update simulation experiments

--- simExpmt2/estRhoBeta-2-2/quick_start.sh
+++
@@ -1,1 +0,0 @@
-sbatch --wrap="bash prepare.sh --nsims 200 --seqlength 1;bash submit_all_runs.sh --nsims 200"
--- simExpmt3/estRhoBeta-2-2/quick_start.sh
+++
@@ -1,1 +0,0 @@
-sbatch --wrap="bash prepare.sh --nsims 200 --seqlength 500;bash submit_all_runs.sh --nsims 200"
