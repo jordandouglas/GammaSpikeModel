095d3f4d2075ed8d1c7f32fdb43849b3f081fc42
robertHyuan <101582763+sgtRoberty@users.noreply.github.com>
2026-01-07 14:39:39 +0100

    Remove simulations

--- simulations/simExpmt3_exactNrStubs/fixedRhoBeta-2-2_drs/quick_start.sh
+++
@@ -1,2 +0,0 @@
-sbatch --wrap="bash prepare.sh --nsims 100;bash submit_all_runs.sh --nsims 100 --seqlength 1000"
-# bash prepare.sh --nsims 10
--- simulations/simExpmt_condOnOrigin/bdpsi_threeNrStubs/quick_start.sh
+++
@@ -1,1 +0,0 @@
-sbatch --wrap="bash prepare.sh --nsims 100;bash submit_all_runs.sh --nsims 100 --seqlength 1000"
